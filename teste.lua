a7cc8a3a984071b27b18a0f9aa98f88ffb397bb1
NullPoiinter <79727993+NullPoiinter@users.noreply.github.com>
2022-07-22 11:18:50 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -3,8 +3,11 @@ repeat task.wait() until game:IsLoaded()
 
 game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_travelling_merchant_item:InvokeServer("star_remnant307129")
 if game.PlaceId == 8304191830 then
-	game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_travelling_merchant_item:InvokeServer("star_remnant307129")
+	game.Players.LocalPlayer.PlayerGui.MessageGui.Enabled = false
 	if game.Players.LocalPlayer.Character.Head:FindFirstChild("_overhead") then
 		workspace[game.Players.LocalPlayer.Name].Head["_overhead"]:Destroy()
+	end
+	while wait() do
+	   game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_travelling_merchant_item:InvokeServer("star_remnant307129")
 	end
 end
