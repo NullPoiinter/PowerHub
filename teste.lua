a3609b193bccaf2f17e7cd3ee6c91e838f234548
NullPoiinter <79727993+NullPoiinter@users.noreply.github.com>
2022-07-22 11:19:27 -0300

    Update teste.lua

--- teste.lua
+++ teste.lua
@@ -7,7 +7,7 @@ if game.PlaceId == 8304191830 then
 	if game.Players.LocalPlayer.Character.Head:FindFirstChild("_overhead") then
 		workspace[game.Players.LocalPlayer.Name].Head["_overhead"]:Destroy()
 	end
-	while wait() do
+	while wait() do -- buy item merchant
 	   game:GetService("ReplicatedStorage").endpoints.client_to_server.buy_travelling_merchant_item:InvokeServer("star_remnant307129")
 	end
 end
